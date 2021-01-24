f80bea5222942c73704f532d3979e845e445ab42
IEvgenii Korniichuk <nutscracker.ua@gmail.com>
2021-01-24 17:09:47 +0100

    updated logs

--- heketi/entrypoint.sh
+++ heketi/entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-
-heketi --config=/etc/heketi/heketi.json --disable-auth=true &
+echo "Starting heketi"
+heketi --config=/etc/heketi/heketi.json &
 
 PID="$!"
+echo "Service is running pid: ${PID}"
 
-heketi-cli topology load --json=/etc/heketi/topology.json > /dev/stdout
-
-tail -f /proc/$PID/fd/1
+heketi-cli topology load --json=/etc/heketi/topology.json
+echo "topoloty loaded"
+FILE=/etc/docker
+if [ ! -f "/proc/$PID/fd/1" ]; then
+    echo "tail for pid logs" 
+    tail -f /proc/$PID/fd/1
+else
+    echo "no log file defined"
+    tail -f /entrypoint.sh
+fi
